2e49e1ded0422de6ff353aea45404de4acd409c4
FengchunFan <ffan005@ucr.edu>
2023-05-20 20:20:57 -0700

    finished Backward Elimination

--- Part1/feature.py
+++ Part1/feature.py
@@ -15,8 +15,9 @@ def evaluation():
     return random.randint(1,100)/100
 
 def Forward_Selection(num_feature):
-    print("choose 1")
+    print("You have chosen Forward Selection Method")
     highest_accuracy = evaluation()
+    print()
     print("Using no features and \"random\" evalution, I got an accuracy of", highest_accuracy, "%")
     print()
     print("Beginning search.")
@@ -41,14 +42,52 @@ def Forward_Selection(num_feature):
             best_feature = temp_best_feature
         else:
             print("(Warning, Accuracy has decreased!)")
-            done = True
+            print()
         level += 1
         if(level == num_feature):
             done = True
     print("Finished search!! The best feature subset is", best_feature, "which has an accuracy of", highest_accuracy, "%")
 
 def Backward_Elimination(num_feature):
-    print("choose 2")
+    print("You have chosen Backward_Elimination Method")
+    highest_accuracy = evaluation()
+    print()
+    print("Using all features and \"random\" evalution, I got an accuracy of", highest_accuracy, "%")
+    print()
+    print("Beginning search.")
+    print()
+    done = False
+    level = 0
+    best_feature = []
+    for i in range(1, num_feature+1):
+        best_feature = best_feature + [i]
+    
+    while(done == False):
+        temp_highest_accuracy = 0
+        temp_best_feature = []
+        for i in range(1, num_feature + 1):
+            copy_feature = best_feature.copy()
+            if i in copy_feature:
+                copy_feature.remove(i)
+                temp_node = Node(copy_feature, evaluation())
+                print("using feature(s)", temp_node.get_features(), "accuracy is", temp_node.get_evaluation(), "%")
+                if(temp_node.get_evaluation() > temp_highest_accuracy):
+                    temp_highest_accuracy = temp_node.get_evaluation()
+                    temp_best_feature = temp_node.get_features()
+        print("Feature set", temp_best_feature, "was best, accuracy is", temp_highest_accuracy, "%")
+        print()
+        if(temp_highest_accuracy >= highest_accuracy):
+            highest_accuracy = temp_highest_accuracy
+            best_feature = temp_best_feature
+        else:
+            print("(Warning, Accuracy has decreased!)")
+            print()
+        level += 1
+        if(level == num_feature):
+            done = True
+    
+    print("Finished search!! The best feature subset is", best_feature, "which has an accuracy of", highest_accuracy, "%")
+
 
 print("Welcome to (Fengchun Fan, ffan005, 01)'s Feature Selection Algorithm")
 num_feature = int(input("Please enter total number of features: "))
