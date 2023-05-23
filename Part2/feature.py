b78556f7335a7a845e2670203946694bb097e2b5
FengchunFan <ffan005@ucr.edu>
2023-05-22 17:40:35 -0700

    convert file data to dataset

--- Part2/feature.py
+++ Part2/feature.py
@@ -12,15 +12,21 @@ class Node:
         return self.evaluation
 
 #need real evaluation functions, return accuracy calculated through NN classifier
-def evaluation(feature, file_name):
+def evaluation(feature, dataset, num_instances):
     #print("working on", file_name, "with features:", feature)
-    return len(feature)
+    total_correct = 0
+    for i in range (num_instances):
+        test_index = i
+        predicted_label = 0
+    #eva_accuracy = round(random.uniform(0, 100), 2)
+    eva_accuracy = total_correct/num_instances
+    return eva_accuracy
 
-def Forward_Selection(num_feature, file_name):
+def Forward_Selection(num_feature, dataset, num_instances):
     print("You have chosen Forward Selection Method")
     feature = []
     best_feature = []
-    highest_accuracy = evaluation(feature, file_name)
+    highest_accuracy = evaluation(feature, dataset, num_instances)
     print()
     print("Using no features and \"random\" evalution, I got an accuracy of", highest_accuracy, "%")
     print()
@@ -34,7 +40,7 @@ def Forward_Selection(num_feature, file_name):
         temp_best_feature = []
         for i in range(1, num_feature + 1):
             if i not in feature:
-                temp_node = Node([i] + feature, evaluation([i] + feature, file_name))
+                temp_node = Node([i] + feature, evaluation([i] + feature, dataset, num_instances))
                 print("using feature(s)", temp_node.get_features(), "accuracy is", temp_node.get_evaluation(), "%")
                 if(temp_node.get_evaluation() > temp_highest_accuracy):
                     temp_highest_accuracy = temp_node.get_evaluation()
@@ -54,14 +60,14 @@ def Forward_Selection(num_feature, file_name):
             done = True
     print("Finished search!! The best feature subset is", best_feature, "which has an accuracy of", highest_accuracy, "%")
 
-def Backward_Elimination(num_feature, file_name):
+def Backward_Elimination(num_feature, dataset, num_instances):
     print("You have chosen Backward_Elimination Method")
     feature = []
     best_feature = []
     for i in range(1, num_feature+1):
         feature = feature + [i] 
         best_feature = best_feature + [i]
-    highest_accuracy = evaluation(feature, file_name)
+    highest_accuracy = evaluation(feature, dataset, num_instances)
     print()
     print("Using all features and \"random\" evalution, I got an accuracy of", highest_accuracy, "%")
     print()
@@ -77,7 +83,7 @@ def Backward_Elimination(num_feature, file_name):
             copy_feature = feature.copy()
             if i in copy_feature:
                 copy_feature.remove(i)
-                temp_node = Node(copy_feature, evaluation(copy_feature, file_name))
+                temp_node = Node(copy_feature, evaluation(copy_feature, dataset, num_instances))
                 print("using feature(s)", temp_node.get_features(), "accuracy is", temp_node.get_evaluation(), "%")
                 if(temp_node.get_evaluation() > temp_highest_accuracy):
                     temp_highest_accuracy = temp_node.get_evaluation()
@@ -103,18 +109,27 @@ print("Welcome to (Fengchun Fan, ffan005, 01)'s Feature Selection Algorithm")
 #num_feature = int(input("Please enter total number of features: "))
 file_name = input("Please enter the name of the dataset you want to work on: ")
 num_instances = 0
+dataset = []
 with open(file_name, "r") as file:
     one_line = file.readline() #first line
     num_feature = one_line.count(".") - 1 #get number of features in the dataset
+    values = one_line.split()
+    parsed_data = [float(value) for value in values]
+    dataset.append(parsed_data)
     num_instances += 1
     for line in file:
         num_instances += 1
+        values = line.split()
+        parsed_data = [float(value) for value in values]
+        dataset.append(parsed_data)
 file.close()
 print()
 print("You have chosen", file_name)
 print("total number of features in this dataset is:", num_feature)
 print("total number of instances in this dataset is:", num_instances)
+print("size of the dataset to double check:", len(dataset), "x", len(dataset[0]))
 print()
+#print(dataset[1][0])
 
 print("Type the number of the algorithm you want to run.")
 print("1. Forward Selection")
@@ -124,8 +139,8 @@ selected_algorithm = 0
 while(selected_algorithm != 1 and selected_algorithm != 2):
     selected_algorithm = int(input())
     if(selected_algorithm == 1):
-        Forward_Selection(num_feature, file_name)
+        Forward_Selection(num_feature, dataset, num_instances)
     elif(selected_algorithm == 2):
-        Backward_Elimination(num_feature, file_name)
+        Backward_Elimination(num_feature, dataset, num_instances)
     else:
         print("invalid choice")
