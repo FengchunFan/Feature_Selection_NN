ffc58075baf81f35138a709404ce2064292e493b
FengchunFan <77016886+FengchunFan@users.noreply.github.com>
2023-06-07 17:17:36 -0700

    Update main.cpp

--- C++/main.cpp
+++ C++/main.cpp
@@ -106,171 +106,7 @@ void greedyForward_s(int featureNum, double (&array)[][11]) {
     }
     cout << "} which has an accuracy of " << highest_accuracy << "%\n";
 }
-/*
-double greedyBackward(int featureNum)
-{
-    double bestOutcome = 0;
-    double max = 0;
-    double oldMax;
-    int maxFeatureIndex = 0;
-    int index;
-    double accuracy;
-    vector<int> num;
-    vector<double> accuracys;
-    vector<int> path;
-    vector<double> allMax;
-    int totalindex = 0;
-    bool maxFlag = false;
-    int totalRun = featureNum;
-    if (featureNum == 0)
-    {
-        bestOutcome = eval();
-        return bestOutcome;
-    }
-    for (int i = 0; i < featureNum; i++)
-    {
-        num.push_back(i + 1);
-    }
-    cout << "\nBeginning search.\n\n";
-    for (int j = 0; j < totalRun; j++)
-    {
-        if (featureNum == 0)
-        {
-            break;
-        }
-        j = 0;
-        index = 0;
-        max = 0;
-        maxFlag = false;
-        for (int k = num.size() - 1; k >= 0; k--)
-        {
-            accuracy = eval();
-            if (accuracy > max)
-            {
-                max = accuracy;
-                index = k;
-                if (max > oldMax)
-                {
-                    maxFlag = true;
-                    maxFeatureIndex++;
-                }
-            }
-            cout << "Using feature(s) {" << num[k] << "} accuracy is " << accuracy << "\n";
-        }
-        totalindex++;
-        if (maxFlag == false)
-        {
-            cout << "(Warning, Accuracy has decreased!)\n";
-        }
-        path.push_back(num[index]);
-        cout << "Feature set {";
-        for (int m = 0; m < totalindex; m++)
-        {
-            if (m == totalindex - 1)
-            {
-                cout << path[m];
-            }
-            else
-            {
-                cout << path[m] << ",";
-            }
-        }
-        cout << "} was best, accuracy is " << max << "\n";
-        num.erase(num.begin() + index);
-        num.erase(remove(num.begin(), num.end(), 0), num.end());
-        featureNum--;
-        oldMax = max;
-        allMax.push_back(max);
-    }
 
-    for (int i = 0; i < allMax.size(); i++)
-    {
-        if (allMax[i] > bestOutcome)
-        {
-            bestOutcome = allMax[i];
-            maxFeatureIndex = i + 1;
-        }
-    }
-
-    cout << "FINISHED SEARCH!! The best feature subset is {";
-    for (int m = 0; m < maxFeatureIndex; m++)
-    {
-        if (maxFeatureIndex == 0)
-        {
-            cout << path[m];
-        }
-        else
-        {
-            if (m == maxFeatureIndex - 1)
-            {
-                cout << path[m];
-            }
-            else
-            {
-                cout << path[m] << ",";
-            }
-        }
-    }
-    cout << "} which has an accuracy of " << bestOutcome << "\n";
-    return bestOutcome;
-}*/
-
-void normal2(double (&arr)[][41], int row)
-{
-    double min = 44444;
-    double max = -44444;
-    //This portion here calculated the means of every feature so the columns excluding the first column
-    for(int i = 1; i < 41;i++)
-    {   
-        for(int j = 0; j < row; j++)
-        {
-            if(arr[j][i-1] < min)
-            {
-                min = arr[j][i-1];
-            }
-            else if(arr[j][i-1]>max)
-            {
-                max = arr[j][i-1];
-            }
-        }
-    }
-    double denom = max-min;
-    for(int i = 1; i < 41;i++)
-    {   
-        for(int j = 0; j < row; j++)
-        {
-            arr[j][i] = (arr[j][i] - min)/denom;
-        }
-    }
-}
-void normal(double (&arr)[][11], int row)
-{
-    double min = 44444;
-    double max = -44444;
-    //This portion here calculated the means of every feature so the columns excluding the first column
-    for(int i = 1; i < 11;i++)
-    {   
-        for(int j = 0; j < row; j++)
-        {
-            if(arr[j][i-1] < min)
-            {
-                min = arr[j][i-1];
-            }
-            else if(arr[j][i-1]>max)
-            {
-                max = arr[j][i-1];
-            }
-        }
-    }
-    double denom = max-min;
-    for(int i = 1; i < 11;i++)
-    {   
-        for(int j = 0; j < 100; j++)
-        {
-            arr[j][i] = (arr[j][i] - min)/denom;
-        }
-    }
-}
 
 int main()
 {
@@ -289,7 +125,6 @@ int main()
         // File opened successfully
             cout<< "successfully imported small test dataset\n";
         } else {
-        // Error opening the file
             cout << "failed\n";
         }
 
@@ -309,7 +144,6 @@ int main()
                 colcount++;  
             }
         }
-        normal(array, 100);
         cout << "\nType the number of the algorithm you want to run.\n\n";
         cout << "      1) Forward Selection\n      2) Backward Elimination\n      3) Group 11's Special Algorithm.\n\n\n";
         cin >> algNum;
@@ -344,38 +178,8 @@ int main()
                 colcount2++;  
             }
         }
-        normal2(array, 1000); //perform normalization
     }else{
         cout << "invalid input, exit the program" << endl;
         exit(1);
     }
-    /*
-    cout << "\nType the number of the algorithm you want to run.\n\n";
-    cout << "      1) Forward Selection\n      2) Backward Elimination\n      3) Group 11's Special Algorithm.\n\n\n";
-    cin >> algNum;
-    if(algNum == 1)
-    {
-        if(choice == 1){
-            greedyForward_s(featureNum, array);
-        }else{
-            //cout << "Using no features, I get an accuracy of " << greedyForward_l(0, array, choice) << endl;
-            //greedyForward_l(featureNum, array, choice);
-        }
-    }
-    else if(algNum == 2)
-    {
-       //cout << "Using no features and random evalution, I get an accuracy of " << greedyBackward(0);
-       // greedyBackward(featureNum); 
-    }
-
-    
-    //cout << "Using no features and random evalution, I get an accuracy of " << ;
-    for(int i = 0; i < 100; i++)
-    {
-    for(int j = 0; j < 11; j++)
-    {
-        cout << array[i][j] << " ";
-    }
-    cout << endl;
-    */
 }
